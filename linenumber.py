177d37928730559eebbe2f61dad6c5851c84e158
ILoveChenKX <2722520590@qq.com>
2017-11-18 14:45:04 +0800

    Triple kill

--- linenumber.py
+++ linenumber.py
@@ -7,7 +7,7 @@ def solveFile (fileName) :
     file.close();
     for i in range (1, len(codeLines)+1) :
         if i >= 10000 :
-            print("%s %s", str(i-1000).zfill(4), codeLines[i]);
+            print("%s %s"%(str(i-10000).zfill(4), codeLines[i-1]));
         else :
             print("%s %s"%(str(i).zfill(4), codeLines[i-1]));
 
